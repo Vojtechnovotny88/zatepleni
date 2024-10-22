e3c247e609da4c442c2b0bfbb381097a2fa32468
Vojtěch Novotný <Vojta@MacBook-Air.local>
2024-10-22 11:37:09 +0200

    Servírování HTML kalkulátoru přes Flask

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return "Hello, world! Tohle je moje první aplikace!"
+def index():
+    return render_template('print_offer.html')  # Změň na název tvého HTML souboru
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
